4d215b5662d3b9c4feba189ec509ca215fdd35ee
tgrogers <timrogers@purdue.edu>
2017-05-17 18:09:04 -0400

    getting rid of the test

--- install_torque.sh
+++ install_torque.sh
@@ -35,15 +35,3 @@ echo "`hostname` np=2" | sudo tee -a /var/spool/torque/server_priv/nodes
 sudo /opt/local/sbin/pbs_server 
 sudo /etc/init.d/pbs_mom start
 sudo /etc/init.d/pbs_sched start
-
-
-cd run
-/opt/local/bin/qsub torque.sim
-
-/opt/local/bin/qstat
-
-
-sleep 5
-
-ls ~/run
-cat ~/run/bar3.txt
